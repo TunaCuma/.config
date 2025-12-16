7a26203e6457a6c629944ac559ba17c4fbe003f1
Tuna Cuma <tuna.cuma@udemy.com>
2025-12-16 13:02:56 +0300

    removed nvim

--- nvim-old/lua/custom/configs/lspconfig.lua
+++
@@ -1,5 +0,0 @@
--- Main LSP configuration file
--- This file now just delegates to the modular LSP configuration
-
--- Load and initialize all LSP configurations
-return require("custom.configs.lsp").setup()
